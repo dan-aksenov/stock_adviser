f5cc2a75b3263b3a5deeec966f8caa89655d9ab2
dan-aksenov <dan-aksenov@yandex.ru>
2018-02-16 17:30:07 +0300

    more charts added

--- analizer.py
+++ analizer.py
@@ -45,3 +45,68 @@ def main_chart( ticker ):
     #chart.render_in_browser()
     #chart.render_to_file('/tmp/chart.svg')
     #chart.render_to_png('/tmp/chart.svg')
+
+'''
+def Elder_FI():
+    ticker = request.args[0]
+    db_data = db(db.stock_w_fi.ticker == ticker).select(db.stock_w_fi.dt,db.stock_w_fi.fi2,db.stock_w_fi.fi13)
+
+    close_dates = []
+    fi2=[]
+    fi13=[]
+
+    for row in db_data:
+        close_dates.append(row.dt)
+        fi2.append(row.fi2)
+        fi13.append(row.fi13)
+
+    response.files.append(URL('default','static/js/pygal-tooltips.min.js'))
+    response.headers['Content-Type']='image/svg+xml'
+    import pygal
+    from pygal.style import CleanStyle
+    chart = pygal.Line(style=CleanStyle)
+    chart.x_labels = (map(lambda d: d.strftime('%Y-%m-%d'), close_dates))
+    chart.add('fi2', fi2)
+    chart.add('fi13', fi13)
+    return chart.render()
+
+def AO():
+    ticker = request.args[0]
+    db_data = db(db.stock_w_fi.ticker == ticker).select(db.stock_w_fi.dt,db.stock_w_fi.ao)
+
+    close_dates = []
+    ao=[]
+
+    for row in db_data:
+        close_dates.append(row.dt)
+        ao.append(row.ao)
+
+    response.files.append(URL('default','static/js/pygal-tooltips.min.js'))
+    response.headers['Content-Type']='image/svg+xml'
+    import pygal
+    from pygal.style import CleanStyle
+    chart = pygal.Bar(style=CleanStyle)
+    chart.x_labels = (map(lambda d: d.strftime('%Y-%m-%d'), close_dates))
+    chart.add('AO', ao)
+    return chart.render()
+
+def volume():
+    ticker = request.args[0]
+    db_data = db(db.stock_w_fi.ticker == ticker).select(db.stock_w_fi.dt,db.stock_w_fi.volume)
+
+    close_dates = []
+    vol=[]
+
+    for row in db_data:
+        close_dates.append(row.dt)
+        vol.append(row.volume)
+
+    response.files.append(URL('default','static/js/pygal-tooltips.min.js'))
+    response.headers['Content-Type']='image/svg+xml'
+    import pygal
+    from pygal.style import CleanStyle
+    chart = pygal.Bar(style=CleanStyle)
+    chart.x_labels = (map(lambda d: d.strftime('%Y-%m-%d'), close_dates))
+    chart.add('Volume', vol)
+    return chart.render()
+'''
