fc26ee899e51840094ea98db637a5dd073b0d15a
dan-aksenov <dan-aksenov@yandex.ru>
2018-03-02 16:53:14 +0300

    fixes

--- dash_app2.py
+++ dash_app2.py
@@ -15,24 +15,28 @@ app.layout = html.Div([
     html.H1('Stock Tickers'),
     dcc.Dropdown(
         id='my-dropdown',
-        options=[{'label': ticker, 'value': ticker		}
+        options=[{'label': ticker, 'value': ticker        }
                  for ticker in tickers],
         value='SBER'
     ),
-    dcc.Graph(id='close+emas chart')
+    #dcc.Graph(id='close+emas chart')
+    html.Div(id='close+emas chart')
 ])
 
-@app.callback(Output('close+emas chart', 'figure'), [Input('my-dropdown', 'value')])
+@app.callback(Output('close+emas chart', 'children'), [Input('my-dropdown', 'value')])
 def update_graph(selected_dropdown_value):
     stock_data = analizer.get_data( selected_dropdown_value )
     graph_data = {    
-	        'data': [
+            'data': [
                 {'x': stock_data[2],'y': stock_data[1], 'type': 'line', 'name': 'close'},
                 {'x': stock_data[2],'y': stock_data[3], 'type': 'line', 'name': 'ema10'},
                 {'x': stock_data[2],'y': stock_data[4], 'type': 'line', 'name': 'ema20'},
                 ] 
-			}
-    return graph_data
-			
+            }
+    graph = dcc.Graph(
+        id=ticker,
+        figure= graph_data)
+    return graph
+            
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0')
