56cb9adfabc6e5a4d6b9380981e11d79153d7063
dan-aksenov <dan-aksenov@yandex.ru>
2018-03-02 15:48:39 +0300

    updated

--- utils.bash
+++ utils.bash
@@ -3,4 +3,4 @@ pg_dump --format plain --schema="stocker" --clean --schema-only --verbose --file
 
 psql -f schema.sql -U stocker stocker
 
-psql -U stocker stocker -c "copy.."
+psql -U stocker stocker -c "copy stock_hist(dt,ticker,open,close,low,high,volume) FROM '/file'"
