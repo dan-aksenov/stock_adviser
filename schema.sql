9dcc5b6b5642bf7239e378d9cf51c8c8edebd599
dan-aksenov <dan-aksenov@yandex.ru>
2018-03-02 15:37:40 +0300

    schema redone

--- schema.sql
+++ schema.sql
@@ -2,105 +2,49 @@
 -- PostgreSQL database dump
 --
 
+-- Dumped from database version 9.6.5
+-- Dumped by pg_dump version 9.6.5
+
+-- Started on 2018-03-02 15:34:43
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
+SET row_security = off;
 
 SET search_path = stocker, pg_catalog;
 
-ALTER TABLE ONLY stocker.auth_permission DROP CONSTRAINT auth_permission_group_id_fkey;
-ALTER TABLE ONLY stocker.auth_membership DROP CONSTRAINT auth_membership_user_id_fkey;
-ALTER TABLE ONLY stocker.auth_membership DROP CONSTRAINT auth_membership_group_id_fkey;
-ALTER TABLE ONLY stocker.auth_event DROP CONSTRAINT auth_event_user_id_fkey;
-ALTER TABLE ONLY stocker.auth_cas DROP CONSTRAINT auth_cas_user_id_fkey;
 ALTER TABLE ONLY stocker.stock_hist DROP CONSTRAINT stock_hist_pkey;
-ALTER TABLE ONLY stocker.image DROP CONSTRAINT image_pkey;
-ALTER TABLE ONLY stocker.auth_user DROP CONSTRAINT auth_user_pkey;
-ALTER TABLE ONLY stocker.auth_permission DROP CONSTRAINT auth_permission_pkey;
-ALTER TABLE ONLY stocker.auth_membership DROP CONSTRAINT auth_membership_pkey;
-ALTER TABLE ONLY stocker.auth_group DROP CONSTRAINT auth_group_pkey;
-ALTER TABLE ONLY stocker.auth_event DROP CONSTRAINT auth_event_pkey;
-ALTER TABLE ONLY stocker.auth_cas DROP CONSTRAINT auth_cas_pkey;
 ALTER TABLE stocker.stock_hist ALTER COLUMN id DROP DEFAULT;
-ALTER TABLE stocker.image ALTER COLUMN id DROP DEFAULT;
-ALTER TABLE stocker.auth_user ALTER COLUMN id DROP DEFAULT;
-ALTER TABLE stocker.auth_permission ALTER COLUMN id DROP DEFAULT;
-ALTER TABLE stocker.auth_membership ALTER COLUMN id DROP DEFAULT;
-ALTER TABLE stocker.auth_group ALTER COLUMN id DROP DEFAULT;
-ALTER TABLE stocker.auth_event ALTER COLUMN id DROP DEFAULT;
-ALTER TABLE stocker.auth_cas ALTER COLUMN id DROP DEFAULT;
 DROP VIEW stocker.stock_w_fi_2;
 DROP VIEW stocker.stock_w_fi;
 DROP VIEW stocker.stock_w_ema;
 DROP SEQUENCE stocker.stock_hist_id_seq;
 DROP TABLE stocker.stock_hist;
-DROP SEQUENCE stocker.image_id_seq;
-DROP TABLE stocker.image;
-DROP SEQUENCE stocker.auth_user_id_seq;
-DROP TABLE stocker.auth_user;
-DROP SEQUENCE stocker.auth_permission_id_seq;
-DROP TABLE stocker.auth_permission;
-DROP SEQUENCE stocker.auth_membership_id_seq;
-DROP TABLE stocker.auth_membership;
-DROP SEQUENCE stocker.auth_group_id_seq;
-DROP TABLE stocker.auth_group;
-DROP SEQUENCE stocker.auth_event_id_seq;
-DROP TABLE stocker.auth_event;
-DROP SEQUENCE stocker.auth_cas_id_seq;
-DROP TABLE stocker.auth_cas;
 DROP AGGREGATE stocker.ema(double precision, numeric);
 DROP AGGREGATE stocker.ema(numeric);
 DROP FUNCTION stocker.ema_func(state numeric, inval numeric, alpha numeric);
 DROP FUNCTION stocker.ema_func(state numeric, inval double precision, alpha numeric);
 DROP FUNCTION stocker.ema_func(numeric, numeric);
-DROP EXTENSION plpgsql;
 DROP SCHEMA stocker;
-DROP SCHEMA public;
 --
--- Name: public; Type: SCHEMA; Schema: -; Owner: postgres
---
-
-CREATE SCHEMA public;
-
-
-ALTER SCHEMA public OWNER TO postgres;
-
---
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: postgres
---
-
-COMMENT ON SCHEMA public IS 'standard public schema';
-
-
---
--- Name: stocker; Type: SCHEMA; Schema: -; Owner: pi
+-- TOC entry 3 (class 2615 OID 25684)
+-- Name: stocker; Type: SCHEMA; Schema: -; Owner: stocker
 --
 
 CREATE SCHEMA stocker;
 
 
-ALTER SCHEMA stocker OWNER TO pi;
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
+ALTER SCHEMA stocker OWNER TO stocker;
 
 SET search_path = stocker, pg_catalog;
 
 --
+-- TOC entry 191 (class 1255 OID 25685)
 -- Name: ema_func(numeric, numeric); Type: FUNCTION; Schema: stocker; Owner: stocker
 --
 
@@ -123,6 +67,7 @@ $_$;
 ALTER FUNCTION stocker.ema_func(numeric, numeric) OWNER TO stocker;
 
 --
+-- TOC entry 192 (class 1255 OID 25686)
 -- Name: ema_func(numeric, double precision, numeric); Type: FUNCTION; Schema: stocker; Owner: stocker
 --
 
@@ -141,6 +86,7 @@ $$;
 ALTER FUNCTION stocker.ema_func(state numeric, inval double precision, alpha numeric) OWNER TO stocker;
 
 --
+-- TOC entry 205 (class 1255 OID 25687)
 -- Name: ema_func(numeric, numeric, numeric); Type: FUNCTION; Schema: stocker; Owner: stocker
 --
 
@@ -159,6 +105,7 @@ $$;
 ALTER FUNCTION stocker.ema_func(state numeric, inval numeric, alpha numeric) OWNER TO stocker;
 
 --
+-- TOC entry 580 (class 1255 OID 25688)
 -- Name: ema(numeric); Type: AGGREGATE; Schema: stocker; Owner: stocker
 --
 
@@ -171,6 +118,7 @@ CREATE AGGREGATE ema(numeric) (
 ALTER AGGREGATE stocker.ema(numeric) OWNER TO stocker;
 
 --
+-- TOC entry 581 (class 1255 OID 25689)
 -- Name: ema(double precision, numeric); Type: AGGREGATE; Schema: stocker; Owner: stocker
 --
 
@@ -187,258 +135,8 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: auth_cas; Type: TABLE; Schema: stocker; Owner: stocker; Tablespace: 
---
-
-CREATE TABLE auth_cas (
-    id integer NOT NULL,
-    user_id integer,
-    created_on timestamp without time zone,
-    service character varying(512),
-    ticket character varying(512),
-    renew character(1)
-);
-
-
-ALTER TABLE auth_cas OWNER TO stocker;
-
---
--- Name: auth_cas_id_seq; Type: SEQUENCE; Schema: stocker; Owner: stocker
---
-
-CREATE SEQUENCE auth_cas_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE auth_cas_id_seq OWNER TO stocker;
-
---
--- Name: auth_cas_id_seq; Type: SEQUENCE OWNED BY; Schema: stocker; Owner: stocker
---
-
-ALTER SEQUENCE auth_cas_id_seq OWNED BY auth_cas.id;
-
-
---
--- Name: auth_event; Type: TABLE; Schema: stocker; Owner: stocker; Tablespace: 
---
-
-CREATE TABLE auth_event (
-    id integer NOT NULL,
-    time_stamp timestamp without time zone,
-    client_ip character varying(512),
-    user_id integer,
-    origin character varying(512),
-    description text
-);
-
-
-ALTER TABLE auth_event OWNER TO stocker;
-
---
--- Name: auth_event_id_seq; Type: SEQUENCE; Schema: stocker; Owner: stocker
---
-
-CREATE SEQUENCE auth_event_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE auth_event_id_seq OWNER TO stocker;
-
---
--- Name: auth_event_id_seq; Type: SEQUENCE OWNED BY; Schema: stocker; Owner: stocker
---
-
-ALTER SEQUENCE auth_event_id_seq OWNED BY auth_event.id;
-
-
---
--- Name: auth_group; Type: TABLE; Schema: stocker; Owner: stocker; Tablespace: 
---
-
-CREATE TABLE auth_group (
-    id integer NOT NULL,
-    role character varying(512),
-    description text
-);
-
-
-ALTER TABLE auth_group OWNER TO stocker;
-
---
--- Name: auth_group_id_seq; Type: SEQUENCE; Schema: stocker; Owner: stocker
---
-
-CREATE SEQUENCE auth_group_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE auth_group_id_seq OWNER TO stocker;
-
---
--- Name: auth_group_id_seq; Type: SEQUENCE OWNED BY; Schema: stocker; Owner: stocker
---
-
-ALTER SEQUENCE auth_group_id_seq OWNED BY auth_group.id;
-
-
---
--- Name: auth_membership; Type: TABLE; Schema: stocker; Owner: stocker; Tablespace: 
---
-
-CREATE TABLE auth_membership (
-    id integer NOT NULL,
-    user_id integer,
-    group_id integer
-);
-
-
-ALTER TABLE auth_membership OWNER TO stocker;
-
---
--- Name: auth_membership_id_seq; Type: SEQUENCE; Schema: stocker; Owner: stocker
---
-
-CREATE SEQUENCE auth_membership_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE auth_membership_id_seq OWNER TO stocker;
-
---
--- Name: auth_membership_id_seq; Type: SEQUENCE OWNED BY; Schema: stocker; Owner: stocker
---
-
-ALTER SEQUENCE auth_membership_id_seq OWNED BY auth_membership.id;
-
-
---
--- Name: auth_permission; Type: TABLE; Schema: stocker; Owner: stocker; Tablespace: 
---
-
-CREATE TABLE auth_permission (
-    id integer NOT NULL,
-    group_id integer,
-    name character varying(512),
-    table_name character varying(512),
-    record_id integer
-);
-
-
-ALTER TABLE auth_permission OWNER TO stocker;
-
---
--- Name: auth_permission_id_seq; Type: SEQUENCE; Schema: stocker; Owner: stocker
---
-
-CREATE SEQUENCE auth_permission_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE auth_permission_id_seq OWNER TO stocker;
-
---
--- Name: auth_permission_id_seq; Type: SEQUENCE OWNED BY; Schema: stocker; Owner: stocker
---
-
-ALTER SEQUENCE auth_permission_id_seq OWNED BY auth_permission.id;
-
-
---
--- Name: auth_user; Type: TABLE; Schema: stocker; Owner: stocker; Tablespace: 
---
-
-CREATE TABLE auth_user (
-    id integer NOT NULL,
-    first_name character varying(128),
-    last_name character varying(128),
-    email character varying(512),
-    password character varying(512),
-    registration_key character varying(512),
-    reset_password_key character varying(512),
-    registration_id character varying(512)
-);
-
-
-ALTER TABLE auth_user OWNER TO stocker;
-
---
--- Name: auth_user_id_seq; Type: SEQUENCE; Schema: stocker; Owner: stocker
---
-
-CREATE SEQUENCE auth_user_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE auth_user_id_seq OWNER TO stocker;
-
---
--- Name: auth_user_id_seq; Type: SEQUENCE OWNED BY; Schema: stocker; Owner: stocker
---
-
-ALTER SEQUENCE auth_user_id_seq OWNED BY auth_user.id;
-
-
---
--- Name: image; Type: TABLE; Schema: stocker; Owner: stocker; Tablespace: 
---
-
-CREATE TABLE image (
-    id integer NOT NULL,
-    name character varying(512),
-    file character varying(512)
-);
-
-
-ALTER TABLE image OWNER TO stocker;
-
---
--- Name: image_id_seq; Type: SEQUENCE; Schema: stocker; Owner: stocker
---
-
-CREATE SEQUENCE image_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE image_id_seq OWNER TO stocker;
-
---
--- Name: image_id_seq; Type: SEQUENCE OWNED BY; Schema: stocker; Owner: stocker
---
-
-ALTER SEQUENCE image_id_seq OWNED BY image.id;
-
-
---
--- Name: stock_hist; Type: TABLE; Schema: stocker; Owner: stocker; Tablespace: 
+-- TOC entry 186 (class 1259 OID 25690)
+-- Name: stock_hist; Type: TABLE; Schema: stocker; Owner: stocker
 --
 
 CREATE TABLE stock_hist (
@@ -456,6 +154,7 @@ CREATE TABLE stock_hist (
 ALTER TABLE stock_hist OWNER TO stocker;
 
 --
+-- TOC entry 187 (class 1259 OID 25696)
 -- Name: stock_hist_id_seq; Type: SEQUENCE; Schema: stocker; Owner: stocker
 --
 
@@ -470,6 +169,8 @@ CREATE SEQUENCE stock_hist_id_seq
 ALTER TABLE stock_hist_id_seq OWNER TO stocker;
 
 --
+-- TOC entry 2148 (class 0 OID 0)
+-- Dependencies: 187
 -- Name: stock_hist_id_seq; Type: SEQUENCE OWNED BY; Schema: stocker; Owner: stocker
 --
 
@@ -477,6 +178,7 @@ ALTER SEQUENCE stock_hist_id_seq OWNED BY stock_hist.id;
 
 
 --
+-- TOC entry 188 (class 1259 OID 25698)
 -- Name: stock_w_ema; Type: VIEW; Schema: stocker; Owner: stocker
 --
 
@@ -499,6 +201,7 @@ CREATE VIEW stock_w_ema AS
 ALTER TABLE stock_w_ema OWNER TO stocker;
 
 --
+-- TOC entry 189 (class 1259 OID 25703)
 -- Name: stock_w_fi; Type: VIEW; Schema: stocker; Owner: stocker
 --
 
@@ -528,6 +231,7 @@ CREATE VIEW stock_w_fi AS
 ALTER TABLE stock_w_fi OWNER TO stocker;
 
 --
+-- TOC entry 190 (class 1259 OID 25708)
 -- Name: stock_w_fi_2; Type: VIEW; Schema: stocker; Owner: stocker
 --
 
@@ -552,119 +256,16 @@ CREATE VIEW stock_w_fi_2 AS
 ALTER TABLE stock_w_fi_2 OWNER TO stocker;
 
 --
--- Name: id; Type: DEFAULT; Schema: stocker; Owner: stocker
---
-
-ALTER TABLE ONLY auth_cas ALTER COLUMN id SET DEFAULT nextval('auth_cas_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: stocker; Owner: stocker
---
-
-ALTER TABLE ONLY auth_event ALTER COLUMN id SET DEFAULT nextval('auth_event_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: stocker; Owner: stocker
---
-
-ALTER TABLE ONLY auth_group ALTER COLUMN id SET DEFAULT nextval('auth_group_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: stocker; Owner: stocker
---
-
-ALTER TABLE ONLY auth_membership ALTER COLUMN id SET DEFAULT nextval('auth_membership_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: stocker; Owner: stocker
---
-
-ALTER TABLE ONLY auth_permission ALTER COLUMN id SET DEFAULT nextval('auth_permission_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: stocker; Owner: stocker
---
-
-ALTER TABLE ONLY auth_user ALTER COLUMN id SET DEFAULT nextval('auth_user_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: stocker; Owner: stocker
---
-
-ALTER TABLE ONLY image ALTER COLUMN id SET DEFAULT nextval('image_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: stocker; Owner: stocker
+-- TOC entry 2020 (class 2604 OID 25713)
+-- Name: stock_hist id; Type: DEFAULT; Schema: stocker; Owner: stocker
 --
 
 ALTER TABLE ONLY stock_hist ALTER COLUMN id SET DEFAULT nextval('stock_hist_id_seq'::regclass);
 
 
 --
--- Name: auth_cas_pkey; Type: CONSTRAINT; Schema: stocker; Owner: stocker; Tablespace: 
---
-
-ALTER TABLE ONLY auth_cas
-    ADD CONSTRAINT auth_cas_pkey PRIMARY KEY (id);
-
-
---
--- Name: auth_event_pkey; Type: CONSTRAINT; Schema: stocker; Owner: stocker; Tablespace: 
---
-
-ALTER TABLE ONLY auth_event
-    ADD CONSTRAINT auth_event_pkey PRIMARY KEY (id);
-
-
---
--- Name: auth_group_pkey; Type: CONSTRAINT; Schema: stocker; Owner: stocker; Tablespace: 
---
-
-ALTER TABLE ONLY auth_group
-    ADD CONSTRAINT auth_group_pkey PRIMARY KEY (id);
-
-
---
--- Name: auth_membership_pkey; Type: CONSTRAINT; Schema: stocker; Owner: stocker; Tablespace: 
---
-
-ALTER TABLE ONLY auth_membership
-    ADD CONSTRAINT auth_membership_pkey PRIMARY KEY (id);
-
-
---
--- Name: auth_permission_pkey; Type: CONSTRAINT; Schema: stocker; Owner: stocker; Tablespace: 
---
-
-ALTER TABLE ONLY auth_permission
-    ADD CONSTRAINT auth_permission_pkey PRIMARY KEY (id);
-
-
---
--- Name: auth_user_pkey; Type: CONSTRAINT; Schema: stocker; Owner: stocker; Tablespace: 
---
-
-ALTER TABLE ONLY auth_user
-    ADD CONSTRAINT auth_user_pkey PRIMARY KEY (id);
-
-
---
--- Name: image_pkey; Type: CONSTRAINT; Schema: stocker; Owner: stocker; Tablespace: 
---
-
-ALTER TABLE ONLY image
-    ADD CONSTRAINT image_pkey PRIMARY KEY (id);
-
-
---
--- Name: stock_hist_pkey; Type: CONSTRAINT; Schema: stocker; Owner: stocker; Tablespace: 
+-- TOC entry 2022 (class 2606 OID 25715)
+-- Name: stock_hist stock_hist_pkey; Type: CONSTRAINT; Schema: stocker; Owner: stocker
 --
 
 ALTER TABLE ONLY stock_hist
@@ -672,65 +273,16 @@ ALTER TABLE ONLY stock_hist
 
 
 --
--- Name: auth_cas_user_id_fkey; Type: FK CONSTRAINT; Schema: stocker; Owner: stocker
+-- TOC entry 2147 (class 0 OID 0)
+-- Dependencies: 3
+-- Name: stocker; Type: ACL; Schema: -; Owner: stocker
 --
 
-ALTER TABLE ONLY auth_cas
-    ADD CONSTRAINT auth_cas_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth_user(id) ON DELETE CASCADE;
-
-
---
--- Name: auth_event_user_id_fkey; Type: FK CONSTRAINT; Schema: stocker; Owner: stocker
---
-
-ALTER TABLE ONLY auth_event
-    ADD CONSTRAINT auth_event_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth_user(id) ON DELETE CASCADE;
-
-
---
--- Name: auth_membership_group_id_fkey; Type: FK CONSTRAINT; Schema: stocker; Owner: stocker
---
-
-ALTER TABLE ONLY auth_membership
-    ADD CONSTRAINT auth_membership_group_id_fkey FOREIGN KEY (group_id) REFERENCES auth_group(id) ON DELETE CASCADE;
-
-
---
--- Name: auth_membership_user_id_fkey; Type: FK CONSTRAINT; Schema: stocker; Owner: stocker
---
-
-ALTER TABLE ONLY auth_membership
-    ADD CONSTRAINT auth_membership_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth_user(id) ON DELETE CASCADE;
-
-
---
--- Name: auth_permission_group_id_fkey; Type: FK CONSTRAINT; Schema: stocker; Owner: stocker
---
-
-ALTER TABLE ONLY auth_permission
-    ADD CONSTRAINT auth_permission_group_id_fkey FOREIGN KEY (group_id) REFERENCES auth_group(id) ON DELETE CASCADE;
-
-
---
--- Name: public; Type: ACL; Schema: -; Owner: postgres
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
---
--- Name: stocker; Type: ACL; Schema: -; Owner: pi
---
-
-REVOKE ALL ON SCHEMA stocker FROM PUBLIC;
-REVOKE ALL ON SCHEMA stocker FROM pi;
-GRANT ALL ON SCHEMA stocker TO pi;
-GRANT ALL ON SCHEMA stocker TO postgres;
 GRANT ALL ON SCHEMA stocker TO PUBLIC;
+GRANT ALL ON SCHEMA stocker TO postgres;
 
+
+-- Completed on 2018-03-02 15:34:43
 
 --
 -- PostgreSQL database dump complete
