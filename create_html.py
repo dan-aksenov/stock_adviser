0ab40946e1fceca79c631fb546e7d539e6c9f7ef
dan-aksenov <dan-aksenov@yandex.ru>
2018-03-12 18:18:42 +0300

    chart contents added

--- create_html.py
+++ create_html.py
@@ -5,4 +5,16 @@ f = open('d:/tmp/tickers.html', 'w')
 for i in analizer.get_all_tickers():
     f.write("%s\n" % '<button class="tablinks" onclick="openticker(event, ' + i + ')">'+ i +'</button>')
 
+f.close()
+
+f = open('d:/tmp/tickers.html', 'aw')
+
+for i in analizer.get_all_tickers():
+    f.write(
+         '<div id="'+ i +'" class="tabcontent"> \n' +
+         '<object data="graph.svg" type="image/svg+xml"> \n' +
+         '<img src="yourfallback.jpg" /\n>'
+         '</object>\n</div>\n'
+
+f.close
 	
