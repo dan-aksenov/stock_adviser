63cfcd07b250cfa8eff6812a99cc5806f9d002ee
dan-aksenov <dan-aksenov@yandex.ru>
2018-03-12 17:46:28 +0300

    event name fix

--- create_html.py
+++ create_html.py
@@ -3,6 +3,6 @@ import analizer
 f = open('d:/tmp/tickers.html', 'w')
 
 for i in analizer.get_all_tickers():
-    f.write("%s\n" % '<button class="tablinks" onclick="openCity(event, ' + i + ')">'+ i +'</button>')
+    f.write("%s\n" % '<button class="tablinks" onclick="openticker(event, ' + i + ')">'+ i +'</button>')
 
 	
