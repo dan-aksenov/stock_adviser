c16391d093533ce30422c50a05221da13b77219a
dan-aksenov <dan-aksenov@yandex.ru>
2018-05-17 18:34:57 +0000

    windows encod removed

--- daily_load.sh
+++ daily_load.sh
@@ -1,5 +1,5 @@
 # daily reload
 rm /tmp/stocks.csv
-python get_multiple.py -c /dd -b 90 -d /tmp/stocks.csv
-psql -c "truncate table stock_hist" -U stocker stocker
-psql -c "copy stock_hist(dt,ticker,open,close,low,high,volume) FROM '/tmp/stocks.csv'" -U stocker stocker
+python get_multiple.py -c ~/moex.json -b 90 -d /tmp/stocks.csv
+psql -c "truncate table stock_hist" -h pi.home -U stocker stocker
+psql -c "copy stock_hist(dt,ticker,open,close,low,high,volume) FROM '/tmp/stocks.csv'" -h pi.home -U stocker stocker
