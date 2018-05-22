4b6e64287a22088bf71228bf845f16046441df37
dan-aksenov <dan-aksenov@yandex.ru>
2018-05-22 18:03:27 +0300

    html generator added

--- utils.sh
+++ utils.sh
@@ -4,3 +4,8 @@ pg_dump --format plain --schema="stocker" --schema-only --verbose --file schema.
 psql -f schema.sql -U stocker stocker
 
 psql -c "copy stock_hist(dt,ticker,open,close,low,high,volume) FROM '/tmp/stocks.csv'" -U stocker stocker
+
+# Create HTML Report
+psql -U stocker -H -c "select ticker Longs from long" stocker > d:\tmp\stock.html
+psql -U stocker -H -c "select ticker Shorts from short" stocker >> d:\tmp\stock.html
+
