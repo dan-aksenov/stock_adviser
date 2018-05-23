b1e32caaf93bed040ea221cbcd15e67d25582bc2
dan-aksenov <dan-aksenov@yandex.ru>
2018-05-23 20:21:18 +0300

    typo fixed

--- daily_load.sh
+++ daily_load.sh
@@ -3,5 +3,5 @@ rm /tmp/stocks.csv
 python get_multiple.py -c ~/moex.json -b 90 -d /tmp/stocks.csv
 psql -c "truncate table stock_hist" -h pi.home -U stocker stocker
 psql -c "copy stock_hist(dt,ticker,open,close,low,high,volume) FROM '/tmp/stocks.csv'" -h pi.home -U stocker stocker
-psql -h pi.home -U stocker -H -c "select ticker s from long" stocker > /var/www/html/index.html
+psql -h pi.home -U stocker -H -c "select ticker Longs from long" stocker > /var/www/html/index.html
 psql -h pi.home -U stocker -H -c "select ticker Shorts from short" stocker >> /var/www/html/index.html
