201f966fbf2a3abfcd3679b425f7fb45b1e6e0fc
dan-aksenov <dan-aksenov@yandex.ru>
2018-06-05 00:27:59 +0300

    fix

--- daily_load.sh
+++ daily_load.sh
@@ -2,7 +2,7 @@
 # daily reload
 rm /tmp/stocks.csv
 cd ~/projects/stock_adviser/
-/bin/python ~/projects/stock_adviser/get_multiple.py -c ~/moex.json -b 90 -d /tmp/stocks.csv
+/usr/bin/python ~/projects/stock_adviser/get_multiple.py -c ~/moex.json -b 90 -d /tmp/stocks.csv
 psql -c "truncate table stock_hist" -h pi.home -U stocker stocker
 psql -c "copy stock_hist(dt,ticker,open,close,low,high,volume) FROM '/tmp/stocks.csv'" -h pi.home -U stocker stocker
 psql -h pi.home -U stocker -H -c "select ticker Longs from long" stocker > /var/www/html/index.html
